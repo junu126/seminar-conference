3678a0ecabc2cb769e5fbed046f909b744c78fc8
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-07 11:23:34 +0900

    코드를 짤때 사용한 전체적인 템플릿

---
+++ Co_Hackathon/camera/src/component/Template.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import set from '../img/settings.png';
+import set_ from '../img/support.png';
+import './template.css';
+
+const Template = ({onClick_hu, setting_, nav, first,close, main, bar, setting, onClick_close, onChange,gray, easy, normal, hard}) => {
+    return (
+        <div id="Main">
+            <div className={gray}></div>
+            <div className={setting}>
+                <img className="giar" src={set} alt="톱니"/>
+                <p className="set">안전:<input value={easy} className="input" onChange={onChange}></input>°C</p>
+                <p className="set">위험:<input value={normal} className="input" onChange={onChange}></input>°C</p>
+                <p className="set">화재:<input value={hard} className="input" onChange={onChange}></input>°C</p>
+                <div className={close} onClick={onClick_close}>
+                    <span className="save-text">저장/</span>
+                    <span className="close-text">닫기</span>
+                </div>
+            </div>
+            <div className={setting_}>
+                <img className="giar_" src={set_} alt="톱니"/>
+                <p className="set_">관계자1 : 010-1234-5678</p>
+                <p className="set_">관계자2 : 010-2345-6789</p>
+                <p className="set_">관계자3 : 010-3456-7890</p>
+                <div className={close} onClick={onClick_hu}>
+                    <span className="save-text_">저장/</span>
+                    <span className="close-text_">닫기</span>
+                </div>
+            </div>
+            {first}
+            {nav}
+            <div id="middle">
+                {main}
+                {bar}
+            </div>
+        </div>
+    );
+}
+
+export default Template;
